2d19072caf1d4465f2c16389b42957be9d5789bd
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2024-11-28 01:38:27 +0530

    added navbar

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include './partials/header.php' ?>
+</head>
+
+<body>
+    <section class="navbar">
+        <?php include './partials/navbar.php' ?>
+    </section>
+    <section class="content">
+
+    </section>
+
+</body>
+
+</html>
