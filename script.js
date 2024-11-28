fb54bfc293e79cd6d3a784da6cfbb6de67bc3bc5
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2024-11-28 18:08:22 +0530

    added product section

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ document.querySelector("#to-login").addEventListener("click", function () {
   document.querySelector(".popup-reg").classList.remove("active");
   document.querySelector(".popup").classList.add("active");
 });
+
+//rating products
+const stars = document.querySelectorAll(".rating .star");
+
+stars.forEach((star, index) => {
+  star.addEventListener("mouseover", () => {
+    stars.forEach((s, i) => {
+      s.style.color = i <= index ? "#ffa500" : "#cccccc";
+    });
+  });
+
+  star.addEventListener("mouseout", () => {
+    stars.forEach((s) => {
+      s.style.color = "#cccccc";
+    });
+  });
+
+  star.addEventListener("click", () => {
+    stars.forEach((s, i) => {
+      s.style.color = i <= index ? "#ffa500" : "#cccccc";
+    });
+  });
+});
