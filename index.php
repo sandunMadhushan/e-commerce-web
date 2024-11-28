ed8b8e7e1d71ac5f39e40e7608ce80b43ee40368
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2024-11-28 15:36:35 +0530

    added featured products section

--- index.php
+++ index.php
@@ -11,10 +11,48 @@
         <?php include './partials/login-popup.php' ?>
     </section>
     <section class="content">
+        <section class="hero">
+            <div class="hero-text">
+                <h1>ShopEase</h1>
+                <p>Your one-stop shop for all your needs</p>
+                <a href="shop.php">Shop Now</a>
+            </div>
+        </section>
+        <h2 style="margin-left: 50px;">Featured Products</h2>
+        <section class="featured-products">
+            <div class="product-card">
+                <img src="images/backpack01.png" alt="Product 3">
+                <h2>Product 3</h2>
+                <p>LKR 5500.00</p>
+                <div style="align-items: center; justify-content: center;">
+                    <a class="add2cart" href="#">Add to Cart</a>
+                    <i class="material-icons md-12" href="
+                        #">favorite_border</i>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="images/backpack01.png" alt="Product 3">
+                <h2>Product 3</h2>
+                <p>LKR 5500.00</p>
+                <div style="align-items: center; justify-content: center;">
+                    <a class="add2cart" href="#">Add to Cart</a>
+                    <i class="material-icons md-12" href="
+                        #">favorite_border</i>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="images/backpack01.png" alt="Product 3">
+                <h2>Product 3</h2>
+                <p>LKR 5500.00</p>
+                <div style="align-items: center; justify-content: center;">
+                    <a class="add2cart" href="#">Add to Cart</a>
+                    <i class="material-icons md-12" href="
+                        #">favorite_border</i>
+                </div>
+            </div>
+        </section>
 
-    </section>
-
-    <script src="script.js"></script>
+        <script src="script.js"></script>
 </body>
 
 </html>
